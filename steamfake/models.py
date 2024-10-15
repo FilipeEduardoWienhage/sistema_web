8e9fdb5b151d706cbd197aa6a7a9b460c5c417fa
Filipe Eduardo Wienhage <filipe.eduw@hotmail.com>
2024-10-14 22:06:36 -0300

    web

--- steamfake/models.py
+++ steamfake/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# # Create your models here.
 class Categoria(models.Model):
     nome = models.CharField(max_length=23, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+# class Aluno(models.Model):
+#     nome = models.CharField(max_length=10)
+#     nota1 = models.DecimalField(default=0, decimal_places=2, max_digits=4)
+#     nota2 = models.DecimalField(default=0, decimal_places=2, max_digits=4)
+#     nota3 = models.DecimalField(default=0, decimal_places=2, max_digits=4)
+
+# class Curso(models.Model):
+#     nome = models.CharField(max_length=10)
+#     duracao = models.IntegerField(default=0)
+
+# class Turma(models.Model):
+#     data_inicio = models.DateTimeField()
+#     curso = models.ForeignKey(Curso, on_delete=models.CASCADE, blank=True, null=True)
--- steamfake/views.py
+++ steamfake/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpRequest, HttpResponse
 
+from steamfake.models import Categoria
 # Create your views here.
+def categoria_index(request: HttpRequest) -> HttpResponse:
+    categorias = Categoria.objects.all()
+    dados = {
+        "categorias": categorias
+    }
+    return render(request, "categorias/index.html", context=dados)
+
+
+def categoria_cadastro(request: HttpRequest) -> HttpResponse:
+
+    return render(request, "categorias/cadastro.html")
+
+
+def categoria_cadastrar(request: HttpRequest) -> HttpResponse:
+    nome = request.POST.get("nome")
+    categoria = Categoria(nome=nome)
+    categoria.save()
+    return redirect("categorias")
+
+
+def categoria_apagar(request:HttpRequest, id: int) -> HttpResponse:
+    categoria = Categoria.objects.get(pk=id)
+    categoria.delete()
+    return redirect("categorias")
